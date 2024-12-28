3c9847fad324ca5423758f92c6ba49d62ea11315
SonMyeongJin <smj2802@naver.com>
2024-12-28 23:40:25 +0900

    Initial Commit

---
+++ XcodeGithubTest/XcodeGithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGithubTestApp.swift
+//  XcodeGithubTest
+//
+//  Created by 손명진 on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeGithubTestTests/XcodeGithubTestTests.swift
@@ -0,0 +1,16 @@
+//
+//  XcodeGithubTestTests.swift
+//  XcodeGithubTestTests
+//
+//  Created by 손명진 on 12/28/24.
+//
+
+import Testing
+
+struct XcodeGithubTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
